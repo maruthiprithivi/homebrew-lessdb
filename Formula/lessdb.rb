7bef7ffe2af148e6ed791976f58cc84c5e1032d0
Maruthi Prithivirajan <maruthi@Maruthi-Mac.local>
2026-08-29 19:08:15 +0800

    Rename binary to lessdb (no pager conflict) + refreshed checksums

--- Formula/lessdb.rb
+++ Formula/lessdb.rb
@@ -7,19 +7,19 @@ class Lessdb < Formula
   on_macos do
     on_arm do
       url "https://lessdb.pages.dev/dl/lessdb-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "79ee46c0a9e35a7e5dc07ef2cd50c98426ec5fad4404821a3c54a14d9cbb60ed"
+      sha256 "0d64956e1d9cbe6a3206582b484a8a058b153ec3cdaebaea95e9519a75e16317"
     end
   end
 
   on_linux do
     on_intel do
       url "https://lessdb.pages.dev/dl/lessdb-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e8176d0411ac31db56fd7c260aab25d055c73a39adf3cc13f0275d58e1531515"
+      sha256 "823618c7c28ed85d886d1e312f5f45a42c0219316c79a29237ba6d19e5f04922"
     end
   end
 
   def install
-    bin.install "less"
+    bin.install "lessdb"
   end
 
   def caveats
@@ -33,6 +33,6 @@ class Lessdb < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/less --version")
+    assert_match version.to_s, shell_output("#{bin}/lessdb --version")
   end
 end
